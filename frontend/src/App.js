80635d929e42560582fd704bb24850c02e8888a6
user.name <katherine.dorensky@gmail.com>
2025-07-28 18:23:38 -0400

    Commented out browser routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 // // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./Styles/MainStyling.css"
+import "./Styles/MainStyling.css";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Components/HomePage";
 import HotelPage from "./Components/HotelPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 
 function App() {
-
   return (
     // <HotelPage />
-    <>
-      <BrowserRouter>
+    <div>
+      <Navbar/>
+      <HomePage />
+      <Footer/>
+      {/* <BrowserRouter>
         <Navbar/>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/hotels" element={<HotelPage/>}/>
         </Routes>
         <Footer/>
-      </BrowserRouter>
-      </>
-);
+      </BrowserRouter> */}
+    </div>
+  );
 }
 
 export default App;
