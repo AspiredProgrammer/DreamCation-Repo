665fa9c7ee3afb9b2444a736c76b47cd591d5aee
user.name <katherine.dorensky@gmail.com>
2025-11-05 14:16:56 -0500

    itinerary now excluded so only logged in users see it

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
 					</div>
 				</div>
 				<div className="footer-bottom">
-					<p>&copy; 2024 DreamCation. All rights reserved.</p>
+					<p>&copy; 2025 DreamCation. All rights reserved.</p>
 				</div>
 			</div>
 		</footer>
